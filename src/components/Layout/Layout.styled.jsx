55d4434bc48250d2d2d98e503f52c128957dc59c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-04 12:40:25 +0300

    add Layout component

---
+++ src/components/Layout/Layout.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: ${props => props.theme.space[4]}px;
+  padding: ${props => props.theme.space[4]}px;
+`;

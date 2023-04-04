b6d4bf41e659c1276d1791b36cd6e08e91e1d64c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-04 16:01:59 +0300

    add styles to Title

--- src/components/Title/Title.styled.jsx
+++ src/components/Title/Title.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const MainTitle = styled.h1`
   margin-bottom: ${props => props.theme.space[4]}px;
   text-align: center;
-  color: ${props => props.theme.colors.accent};
-  text-shadow: ${props => props.theme.shadows.textShadow};
+  color: ${props => props.theme.colors.primary};
+  text-shadow: 0 1px 1px #fff, 1px 1px 1px #4b1414;
   font-size: ${props => props.theme.fontSizes.l};
 `;
